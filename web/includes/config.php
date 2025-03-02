89e7a7c51d9cb8fc89b3612a678469c08c1b20f5
Jarnster <jarneverlinden4@gmail.com>
2025-03-02 23:16:31 +0100

    Update config.php

--- web/includes/config.php
+++ web/includes/config.php
@@ -11,10 +11,19 @@ $VYOS_API_TIMEOUT = $config['VYOS_API_TIMEOUT'];
 $curl_debugging = $config["CURL_DEBUGGING"];
 $disable_ssl_verify = $config["DISABLE_SSL_VERIFY"];
 $CACHE_LIFETIME = $config["CACHE_LIFETIME"];
+$CURRENT_CONFIG_HASH = $config["CURRENT_CONFIG_HASH"];
 $__VERSIONING_CODE = $config["__VERSIONING_CODE"];
 
 // If there's no existing password hash in the config, generate and save one
 if ($PWD_HASH === null) {
     $PWD_HASH = password_hash($ADMIN_PWD, PASSWORD_DEFAULT);
     set_config_pwd_hash($PWD_HASH); // Store the newly generated hash in the config
+}
+
+if (md5(json_encode($config)) != $CURRENT_CONFIG_HASH) {
+    require_once 'includes/classes/Caching.php';
+    $CURRENT_CONFIG_HASH = md5(json_encode($config));
+    $caching = new Caching("../data/cache", cacheLifetime: $CACHE_LIFETIME);
+    $caching->invalidateRouterCache(get_selected_router_index());
+    set_config_config_hash($CURRENT_CONFIG_HASH); // Store the newly generated hash in the config
 }
